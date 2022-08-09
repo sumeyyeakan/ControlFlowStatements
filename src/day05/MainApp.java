a3b724a58238f617392d4ac80a1368fa8aac61bc
sumeyyeakan <akansumeyye@gmail.com>
2022-08-09 23:32:59 +0300

    Day 5 initialized.

---
+++ src/day05/MainApp.java
@@ -0,0 +1,8 @@
+package day05;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/day05/MainApp2.java
@@ -0,0 +1,7 @@
+package day05;
+
+public class MainApp2 {
+    public static void main(String[] args) {
+
+    }
+}
